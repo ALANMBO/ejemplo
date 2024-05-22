58a7dfb1092817a6aa6cf1355739169cf566f531
DESKTOP-GT8EFK6 <bejaranomauricio@gamil.com>
2024-05-22 11:34:29 -0400

    ejemplo1

---
+++ app.js
@@ -0,0 +1,50 @@
+//variables
+var texto = "MAURICIO";
+var numero = 12;
+var verdadero = true;
+var cualquierCosa = "PUEDO METER LO QUE SEA";
+//CONSTANTES
+var nombre = "alan";
+console.log(texto, numero, verdadero);
+/*Declaración de Variables: Declara una variable llamada nombre y asígnale tu nombre.
+
+Tipos de Datos: Declara una variable llamada edad y asígnale tu edad como un número.
+
+Constantes: Declara una constante llamada PI y asígnale el valor de π (pi) como un número. */
+var nonbre = "ALAN";
+var edad = 20;
+var PI = 3.1416;
+/*Operaciones Matemáticas: Declara dos variables llamadas num1 y num2, asígnale dos números y luego muestra la suma de ambos.
+
+Concatenación de Cadenas: Declara dos variables llamadas saludo y nombre, asígnale un saludo y tu nombre respectivamente, luego concatena las dos variables para imprimir un mensaje de saludo personalizado.
+
+Booleanos: Declara una variable llamada esMayorDeEdad y asígnale un valor booleano que represente si eres mayor de edad o no. */
+//1
+var a = 2;
+var b = 2;
+var c;
+c = a + b;
+console.log(c);
+//2
+var saludo = "hola";
+var nom = "mauricio";
+console.log(saludo, nom);
+//3
+var z = 18;
+if (z >= 18) {
+    console.log("ERES MAYOR");
+}
+else {
+    console.log("ERES MENOR");
+}
+//4
+var g = 0;
+if (a > 0) {
+    console.log("positivo");
+}
+else if (a < 0) {
+    console.log("negativo");
+}
+else {
+    console.log("cero");
+}
---
+++ app.ts
@@ -0,0 +1,60 @@
+//variables
+let texto:string="MAURICIO"
+let numero:number=12
+let verdadero:boolean=true;
+let cualquierCosa :any="PUEDO METER LO QUE SEA"
+//CONSTANTES
+const nombre:String="alan"
+console.log(texto , numero , verdadero)
+
+
+
+/*Declaración de Variables: Declara una variable llamada nombre y asígnale tu nombre.
+
+Tipos de Datos: Declara una variable llamada edad y asígnale tu edad como un número.
+
+Constantes: Declara una constante llamada PI y asígnale el valor de π (pi) como un número. */
+
+let nonbre : string = "ALAN"
+let edad : number = 20;
+const PI : number = 3.1416
+/*Operaciones Matemáticas: Declara dos variables llamadas num1 y num2, asígnale dos números y luego muestra la suma de ambos.
+
+Concatenación de Cadenas: Declara dos variables llamadas saludo y nombre, asígnale un saludo y tu nombre respectivamente, luego concatena las dos variables para imprimir un mensaje de saludo personalizado.
+
+Booleanos: Declara una variable llamada esMayorDeEdad y asígnale un valor booleano que represente si eres mayor de edad o no. */
+
+//1
+let a : number=2;
+let b : number=2;
+let c : number;
+
+c = a + b
+
+console.log(c)
+
+//2
+let saludo : String =  "hola" 
+let nom : String = "mauricio"
+
+console.log(saludo , nom)
+//3
+let z : number = 18;
+
+if(z>=18){
+    console.log("ERES MAYOR")
+}else{
+    console.log("ERES MENOR")
+}
+
+//4
+
+let g : number = 0;
+
+if(a > 0){
+    console.log("positivo")
+}else if(a < 0){
+    console.log("negativo")
+}else{
+    console.log("cero")
+}
